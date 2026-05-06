4f998d7e6c8a77a74211bffe27c7c425acebb3d7
obay r <obay.rihan@student.giu-uni.de>
2026-05-06 18:21:23 +0300

    setup backend structure middleware auth and jwt protection

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,15 +1,82 @@
-exports.register = async (req, res) => {
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+const generateToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: '7d'
+  });
+};
+
+const register = async (req, res) => {
   try {
-    res.json({ message: 'Register route' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    const { name, email, password } = req.body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({
+        message: 'User already exists'
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
   }
 };
 
-exports.login = async (req, res) => {
+const login = async (req, res) => {
   try {
-    res.json({ message: 'Login route' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({
+        message: 'Invalid email or password'
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        message: 'Invalid email or password'
+      });
+    }
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id)
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message
+    });
   }
+};
+
+module.exports = {
+  register,
+  login
 };
