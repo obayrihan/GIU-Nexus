a82f878305fade91392446b29ed939d414def26a
obay r <obay.rihan@student.giu-uni.de>
2026-05-08 19:01:03 +0300

    Revert "Update jobController.js"
    This reverts commit ce4f8c4020eb57a97f901c2e40498b0f10dc2ab5.

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -1,94 +1,15 @@
-const JobPost = require("../models/jobpost");
-
-
 exports.createJob = async (req, res) => {
   try {
-    const job = await JobPost.create({
-      ...req.body,
-      createdBy: req.user._id,
-    });
-
-    res.status(201).json({
-      success: true,
-      job,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
+    res.json({ message: 'Create job' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
 
-
-// ✅ UPDATE JOB
-exports.updateJob = async (req, res) => {
+exports.getJobs = async (req, res) => {
   try {
-    const job = await JobPost.findById(req.params.id);
-
-    if (!job) {
-      return res.status(404).json({
-        success: false,
-        message: "Job not found",
-      });
-    }
-
-    // 🔒 check owner
-    if (job.createdBy.toString() !== req.user._id.toString()) {
-      return res.status(403).json({
-        success: false,
-        message: "Not authorized to update this job",
-      });
-    }
-
-    const updatedJob = await JobPost.findByIdAndUpdate(
-      req.params.id,
-      req.body,
-      { new: true }
-    );
-
-    res.status(200).json({
-      success: true,
-      job: updatedJob,
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
-  }
-};
-
-
-exports.deleteJob = async (req, res) => {
-  try {
-    const job = await JobPost.findById(req.params.id);
-
-    if (!job) {
-      return res.status(404).json({
-        success: false,
-        message: "Job not found",
-      });
-    }
-
-    // 🔒 check owner
-    if (job.createdBy.toString() !== req.user._id.toString()) {
-      return res.status(403).json({
-        success: false,
-        message: "Not authorized to delete this job",
-      });
-    }
-
-    await job.deleteOne();
-
-    res.status(200).json({
-      success: true,
-      message: "Job deleted successfully",
-    });
-  } catch (error) {
-    res.status(500).json({
-      success: false,
-      message: error.message,
-    });
+    res.json({ message: 'Get jobs' });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
   }
 };
