fb7cd182112e7fc63c728a2b9072b0789a75ac15
obay r <obay.rihan@student.giu-uni.de>
2026-05-08 19:00:43 +0300

    Revert "Update jobRoutes.js"
    This reverts commit 59b24fa6abe9fdac01fc5d0d50d21f7635e8fa1d.

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,22 +1,8 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
+const jobController = require('../controllers/jobController');
 
-const {
-  createJob,
-  updateJob,
-  deleteJob,
-} = require("../controllers/jobController");
-
-const { protect, authorize } = require("../middleware/auth");
-
-
-// ✅ CREATE JOB
-router.post("/", protect, authorize("recruiter"), createJob);
-
-// ✅ UPDATE JOB
-router.patch("/:id", protect, authorize("recruiter"), updateJob);
-
-// ✅ DELETE JOB
-router.delete("/:id", protect, authorize("recruiter"), deleteJob);
+router.post('/jobs', jobController.createJob);
+router.get('/jobs', jobController.getJobs);
 
 module.exports = router;
