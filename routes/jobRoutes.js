97ce911e5d9e44951d40e47bed50713ac2a8fc50
hamzamohamed-tech <xxhamzamohamedxx@gmail.com>
2026-05-08 19:03:24 +0300

    Update jobRoutes.js

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -1,8 +1,22 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const jobController = require('../controllers/jobController');
 
-router.post('/jobs', jobController.createJob);
-router.get('/jobs', jobController.getJobs);
+const {
+  createJob,
+  updateJob,
+  deleteJob,
+} = require("../controllers/jobController");
+
+const { protect, authorize } = require("../middleware/auth");
+
+
+// ✅ CREATE JOB
+router.post("/", protect, authorize("recruiter"), createJob);
+
+// ✅ UPDATE JOB
+router.patch("/:id", protect, authorize("recruiter"), updateJob);
+
+// ✅ DELETE JOB
+router.delete("/:id", protect, authorize("recruiter"), deleteJob);
 
 module.exports = router;
