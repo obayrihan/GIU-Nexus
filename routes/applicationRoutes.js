a02789079b76cdcb73e85ef4ff0a2dd2abaa317f
Obay Rihan <obay.rihan@student.giu-uni.de>
2026-05-09 01:25:59 +0300

    Add authentication middleware to job application route

--- routes/applicationRoutes.js
+++ routes/applicationRoutes.js
@@ -3,10 +3,15 @@ const router = express.Router();
 
 const applicationController = require('../controllers/applicationController');
 const { protect } = require('../middleware/auth');
-router.post('/', applicationController.applyToJob);
+router.post(
+  '/',
+  protect,
+  applicationController.applyToJob
+);
 
 router.get(
   '/my-applications',
+  protect,
   applicationController.getApplications
 );
 
