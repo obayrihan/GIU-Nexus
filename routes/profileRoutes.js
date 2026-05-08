6f3c3f0a4867e937add18b5c4ed1f4775852969a
obay r <obay.rihan@student.giu-uni.de>
2026-05-08 23:50:41 +0300

    feat(profile): implement Hugging Face skill extraction

---
+++ routes/profileRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getProfile,
+  updateProfile,
+  extractSkills,
+} = require("../controllers/profileController");
+
+const { protect } = require("../middleware/auth");
+
+router.get("/", protect, getProfile);
+
+router.patch("/", protect, updateProfile);
+
+router.post('/extract-skills', protect, extractSkills);
+
+
+module.exports = router;
