76c0f411893a6af5612c5720f89437d946aa2416
eyadelsafty06-rgb <eyadelsafty06@gmail.com>
2026-05-08 17:42:57 +0300

    Eyad1

--- routes/jobRoutes.js
+++ routes/jobRoutes.js
@@ -5,4 +5,10 @@ const jobController = require('../controllers/jobController');
 router.post('/jobs', jobController.createJob);
 router.get('/jobs', jobController.getJobs);
 
+router.get('/jobs/recommended', protect, jobController.getRecommendedJobs);
+router.get('/jobs/my-jobs', protect, jobController.getMyJobs);
+router.get('/jobs/saved', protect, jobController.getSavedJobs);
+router.post('/jobs/:id/save', protect, jobController.toggleSaveJob);
+
 module.exports = router;
+
