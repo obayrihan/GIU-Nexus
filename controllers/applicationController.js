f4f1ebc495fe35d9d54641befa3890c1148040aa
Obay Rihan <obay.rihan@student.giu-uni.de>
2026-05-09 01:24:12 +0300

    Implement applyToJob function in applicationController
    Added applyToJob function to handle job applications.

--- controllers/applicationController.js
+++ controllers/applicationController.js
@@ -1,5 +1,19 @@
 const Application = require('../models/Application');
 
+exports.applyToJob = async (req, res) => {
+  try {
+    res.status(200).json({
+      success: true,
+      message: 'Apply to job',
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 exports.getApplications = async (req, res) => {
   try {
     const applications = await Application.find({
