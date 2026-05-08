4ca4ab161b35023121feb66a9f3570e6deca52f8
ali4hassan6-web <ali4.hassan6@gmail.com>
2026-05-08 22:50:22 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
-const jwt = require("jsonwebtoken");
-const User = require("../models/User");
 
+// Protect routes (authentication middleware)
 exports.protect = async (req, res, next) => {
   try {
     let token;
@@ -14,107 +13,34 @@ exports.protect = async (req, res, next) => {
       token = req.headers.authorization.split(" ")[1];
     }
 
-// Protect routes (authentication middleware)
-const protect = async (req, res, next) => {
-  let token;
-
-
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
-
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      req.user = await User.findById(decoded.id).select('-password');
-
-      next();
-
-
     if (!token) {
       return res.status(401).json({
         success: false,
-        message: "Not authorized",
+        message: "No token provided",
       });
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    req.user = await User.findById(decoded.id);
+    req.user = await User.findById(decoded.id).select("-password");
 
     next();
   } catch (error) {
-    res.status(401).json({
+    return res.status(401).json({
       success: false,
       message: "Not authorized",
     });
+  }
+};
 
-
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith("Bearer")
-  ) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
-
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      req.user = await User.findById(decoded.id).select("-password");
-
-      return next();
-    } catch (error) {
-      return res.status(401).json({
-        success: false,
+// Role-based authorization middleware
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res.status(403).json({
         message: "Not authorized",
       });
     }
 
-  }
-};
-
-const authorize = (...roles) => {
-  return (req, res, next) => {
-    if (!req.user || !roles.includes(req.user.role)) {
-      return res.status(403).json({
-        message: 'Not authorized'
-      });
-    }
-
-
     next();
   };
 };
-
-module.exports = {
-  protect,
-  authorize
-};
-
-  return res.status(401).json({
-    success: false,
-    message: "No token provided",
-  });
-};
-
-// Role-based authorization middleware
-const authorize = (...roles) => {
-  return (req, res, next) => {
-    if (!req.user || !roles.includes(req.user.role)) {
-      return res.status(403).json({
-        success: false,
-        message: "Forbidden: not authorized",
-      });
-    }
-
-    next();
-  };
-};
-
-module.exports = {
-  protect,
-  authorize,
-};
-};
-
