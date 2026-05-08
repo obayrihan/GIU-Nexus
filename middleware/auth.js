4c431710d9ab0fe63e25228f983a47e764b980ca
Asser16003477 <asserehabkhattab@gmail.com>
2026-05-08 20:37:15 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,36 +1,33 @@
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
+const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
-const protect = async (req, res, next) => {
-  let token;
+exports.protect = async (req, res, next) => {
+  try {
+    let token;
 
-  if (
-    req.headers.authorization &&
-    req.headers.authorization.startsWith('Bearer')
-  ) {
-    try {
-      token = req.headers.authorization.split(' ')[1];
+    if (
+      req.headers.authorization &&
+      req.headers.authorization.startsWith("Bearer")
+    ) {
+      token = req.headers.authorization.split(" ")[1];
+    }
 
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-      req.user = await User.findById(decoded.id).select('-password');
-
-      next();
-
-    } catch (error) {
+    if (!token) {
       return res.status(401).json({
-        message: 'Not authorized'
+        success: false,
+        message: "Not authorized",
       });
     }
-  }
 
-  if (!token) {
-    return res.status(401).json({
-      message: 'No token provided'
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await User.findById(decoded.id);
+
+    next();
+  } catch (error) {
+    res.status(401).json({
+      success: false,
+      message: "Not authorized",
     });
   }
-};
-
-module.exports = {
-  protect
 };
